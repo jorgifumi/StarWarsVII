577a2213f1559de051e8ec2c607948623b84db22
Jorge Lucena <jorgifumi@gmail.com>
2015-11-24 21:58:17 +0100

    interfaz pública del universo

--- StarWarsVII/StarWarsUniverse.swift
+++ StarWarsVII/StarWarsUniverse.swift
@@ -10,15 +10,54 @@ import UIKit
 
 
 // Carta a los Reyes Magos
-// total de personajes
-// El total de las afiliaciones
-// El nombre de una afiliacion
-// Total de personajes para cada afiliación
-// El personaje n de la afiliacion m
 
-let words : Array<String> = ["shut up"]
+
+
+
+
+
 
 class StarWarsUniverse{
     
-    let characters  : Array<StarWarsCharacter> = []
+    //MARK: - Initializacion
+    
+    //MARK: - Private interface
+    
+    //MARK: - Public interface
+    
+    // total de personajes
+    var countCharacters: Int{
+        get{
+            // todo
+            return 42
+        }
+    }
+    
+    var countAffiliations: Int{
+        
+        // El total de las afiliaciones
+        get{
+            // todo
+            return 42
+        }
+    }
+    
+    // Total de personajes para cada afiliación
+    func countCharacters(inAffiliation: StarWarsAffiliation) -> Int{
+        // todo
+        return 42
+    }
+    
+    // El nombre de una afiliacion
+    func affiliationName(atIndex: Int) -> String{
+        
+        // todo
+        return "The Hitchhikers' guide to the Galaxy"
+    }
+    
+    // El personaje n de la afiliacion m
+//    subscript(idx: Int, inAffiliation aff: StarWarsAffiliation) -> StarWarsCharacter{
+//        // todo
+//        
+//    }
 }
